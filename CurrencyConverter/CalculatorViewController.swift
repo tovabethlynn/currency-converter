13bc44f28b46d78650914c582bede1bd56d5073d
Tova <tova.bethlynn@gmail.com>
2017-02-28 15:08:30 -0800

    Removed project description comment

--- CurrencyConverter/CalculatorViewController.swift
+++ CurrencyConverter/CalculatorViewController.swift
@@ -6,16 +6,6 @@
 //  Copyright © 2017 Toes. All rights reserved.
 //
 
-
-/*
- 
- V. Create a currency converter by utilizing data from the fixer.io API.
- The currency converter must include a currency selector at the top to use as the base currency and display updated currency rates based on the selected base currency. When a user taps on a currency, a calculation view should appear with the selected currency and base currency. Only the base currency field should be editable.
- Feel free to use any open source libraries.
- (Consider this project as if you were developing a component within a large-scaled project)
- 
-*/
-
 import UIKit
 import Foundation
 
